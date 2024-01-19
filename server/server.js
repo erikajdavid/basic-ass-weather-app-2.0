6974db74c02e831399a1a18e91cbda1e3dccc1fb
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-19 16:02:57 -0500

    added routes to server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+//register and login routes
+app.use("/auth", require("./routes/jwtAuth"));
+
 app.listen (PORT, () => {
     console.log(`Server is listening on port ${PORT}.`);
 }); 
