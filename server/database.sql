fd29879a7540467e8cf556ff1ecab6035c3822f7
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-19 16:32:22 -0500

    dropped password validation from table

--- server/database.sql
+++ server/database.sql
@@ -4,6 +4,5 @@ CREATE TABLE users(
     user_id UUID DEFAULT uuid_generate_v4(),
     user_email VARCHAR(255) NOT NULL UNIQUE,
     user_password VARCHAR(255) NOT NULL, 
-    user_password_verify VARCHAR(255) NOT NULL, 
     PRIMARY KEY (user_id)
 );
