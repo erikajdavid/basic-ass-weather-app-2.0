39f766e00e33f2b2ad82911e811ab6bf5194e65e
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-19 15:40:47 -0500

    created postgrees database and users table

---
+++ server/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE authweatherapp;
+
+CREATE TABLE users(
+    user_id UUID DEFAULT uuid_generate_v4(),
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL, 
+    user_password_verify VARCHAR(255) NOT NULL, 
+    PRIMARY KEY (user_id)
+);
