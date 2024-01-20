47d5bee4fbb6359586fde58f933c12cf7607268b
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 16:48:58 -0500

    omg trying to get the useEffect to work omggggg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,52 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 
-//import components
+// Import components
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import Landing from "./components/Landing"
+import Landing from "./components/Landing";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuth = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
 
   return (
     <>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <Signup /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
+        />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
