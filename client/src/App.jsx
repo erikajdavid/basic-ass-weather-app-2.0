19192a7aa3814c9d961881786128363f2c3f9868
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 15:52:25 -0500

    import Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css';
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import './App.css';
 
 //import components
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Landing from "./components/Landing"
+import Dashboard from "./components/Dashboard";
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <>
       <Routes>
-      <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
