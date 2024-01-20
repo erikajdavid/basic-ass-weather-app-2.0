92bbd1b86a045e39078caa307e17d179efef8a72
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 15:51:14 -0500

    logout function

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,23 @@
+import React from "react";
+
 const Dashboard = () => {
+
+    const logoutUser = async (e) => {
+        e.preventDefault();
+
+        try {
+            //delete token from localstorage
+            localStorage.removeItem("token");
+            
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     return (
         <>
             <h1>Welcome authenticated user</h1>
+            <button onClick={logoutUser}>Log out</button>
         </>
 
     );
