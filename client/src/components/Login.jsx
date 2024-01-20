dade9ddc30562314b2f9d8492617f81e70935890
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 13:19:37 -0500

    toggle password, updated format, fix loginUser bug

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEyeSlash, faEye } from "@fortawesome/free-regular-svg-icons";
+
 
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [hidePassword, setHidePassword] = useState(true);
+
+    const handlePasswordToggle = () => {
+        setHidePassword(!hidePassword);
+    }
 
     const loginUser = async (e) => {
-        e.preventDefault(e)
+        e.preventDefault()
         try {
             //does stuff
         } catch (error) {
@@ -17,7 +25,7 @@ const Login = () => {
 
     return (
         <>
-            <form onSubmit={loginUser}>
+            <form onSubmit={ loginUser }>
                 <h1>Log in</h1>
 
                 <label htmlFor="email"></label>
@@ -25,22 +33,29 @@ const Login = () => {
                     type="email"
                     name="email"
                     id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required={true}
+                    value={ email }
+                    onChange={ (e) => setEmail(e.target.value) }
+                    required={ true }
                     placeholder="Email:"
                 />
 
-                <label htmlFor="password"></label>
-                <input 
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required={true}
-                    placeholder="Password:"
-                />
+                <div>
+                    <label htmlFor="password"></label>
+                    <input 
+                        type={ hidePassword ? "password" : "text "}
+                        name="password"
+                        id="password"
+                        value={ password }
+                        onChange={ (e) => setPassword(e.target.value) }
+                        required={ true }
+                        placeholder="Password:"
+                    />
+                    <FontAwesomeIcon
+                        onClick={ handlePasswordToggle }
+                        icon={ hidePassword ? faEyeSlash : faEye }
+                    >
+                    </FontAwesomeIcon>
+                </div>
 
                 <button type="submit">Log in</button>
                 <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
