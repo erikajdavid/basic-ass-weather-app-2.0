f90ed1c001ddbda15df22101b7d973a91359fbb9
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 00:02:40 -0500

    states and onChanges

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     return (
         <>
@@ -11,23 +15,32 @@ const Signup = () => {
                     type="email"
                     name="email"
                     id="email"
+                    onValue={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required={true}
                     placeholder="Email:"
                 />
+
                 <input 
                     type="password"
                     name="password"
                     id="password"
+                    onValue={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required={true}
                     placeholder="Password:"
                 />
+
                  <input 
-                    type="passwordConfirm"
-                    name="passwordConfirm"
-                    id="passwordConfirm"
+                    type="confirmPassword"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    onValue={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
                     required={true}
                     placeholder="Confirm password:"
                 />
+                
                 <button type="submit">Register for an account</button>
                 <p>Already have an account? <Link to="/login">Log in</Link></p>
             </form>
