ea04af0fb5e7c4f4fe66438526ec95c47d5342b6
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-19 23:57:49 -0500

    basic form layout

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
 
     return (
         <>
             <h1>Sign up</h1>
+            <form>
+                <input 
+                    type="email"
+                    name="email"
+                    id="email"
+                    required={true}
+                    placeholder="Email:"
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    id="password"
+                    required={true}
+                    placeholder="Password:"
+                />
+                 <input 
+                    type="passwordConfirm"
+                    name="passwordConfirm"
+                    id="passwordConfirm"
+                    required={true}
+                    placeholder="Confirm password:"
+                />
+                <button type="submit">Register for an account</button>
+                <p>Already have an account? <Link to="/login">Log in</Link></p>
+            </form>
         </>
     );
 
