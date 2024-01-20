ca4e3b24c5991f0bd3104c6b3e7b00bfbe977d61
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-20 00:28:44 -0500

    added states, onChange and onSubmit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,47 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUser = async (e) => {
+        e.preventDefault(e)
+        try {
+            //does stuff
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
 
     return (
         <>
             <h1>Log in</h1>
+            <form onSubmit={loginUser}>
+                <input 
+                    type="email"
+                    name="email"
+                    id="email"
+                    onValue={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required={true}
+                    placeholder="Email:"
+                />
+
+                <input 
+                    type="password"
+                    name="password"
+                    id="password"
+                    onValue={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required={true}
+                    placeholder="Password:"
+                />
+
+                <button type="submit">Log in</button>
+                <p>Don't have an account? <Link to="/signup">Sign up</Link>.</p>
+            </form>
         </>
     );
 
