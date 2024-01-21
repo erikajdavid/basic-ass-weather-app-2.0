9cebee47e0b32945d313b093307d9801bb5ca90d
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-21 11:53:18 -0500

    fixed duplicate user bug

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -29,13 +29,17 @@ const Signup = ({ setAuth }) => {
                 body: JSON.stringify(body)
             });
 
-            const parseResponse = await response.json();
-
-            //save token in local storage
-            localStorage.setItem("token", parseResponse.token);
-
-            setAuth(true);
-
+                if (response.ok) {
+                    const parseResponse = await response.json();
+        
+                    // Save token in local storage
+                    localStorage.setItem("token", parseResponse.token);
+        
+                    setAuth(true);
+                } else {
+                    console.error("Registration failed:", response.status, response.statusText);
+                }
+                
             } catch (error) {
                 console.error(error.message);
             }
