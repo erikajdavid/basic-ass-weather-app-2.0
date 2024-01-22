999d34b4c7283bb97902691a2ec7c4d0b5b08acc
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-22 12:02:37 -0500

    get token from authorization header instead

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,24 +1,31 @@
 const jwt = require("jsonwebtoken");
 
-const auth = async(req, res, next) => {
-    const token = req.header("token");
-    
-    //is there no token, return error
-    if (!token) {
-        return res.status(403).json({ message: `Unauthorized.` });
+const auth = async (req, res, next) => {
+    const authHeader = req.header("Authorization");
+
+    //check if there is no Authorization header
+    if (!authHeader) {
+        return res.status(403).json({ message: "Unauthorized. No token provided." });
     }
+
+    //split the header to get the token
+    const [tokenType, token] = authHeader.split(" ");
+
+    //check if the token type is Bearer
+    if (tokenType !== "Bearer" || !token) {
+        return res.status(403).json({ message: "Unauthorized. Invalid token format." });
+    }
+
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
 
         req.user = payload.user;
 
         next();
-
     } catch (error) {
         console.error(error.message);
-        return res.status(403).json({ message: `Unauthorized.` })
+        return res.status(403).json({ message: "Unauthorized. Invalid token." });
     }
-    console.log(token);
 };
 
 module.exports = auth;
