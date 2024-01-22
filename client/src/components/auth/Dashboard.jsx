2d6f6ab712381ac27977e7db081bd2b3ef69089e
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-22 12:43:43 -0500

    cleaned up code

--- client/src/components/auth/Dashboard.jsx
+++ client/src/components/auth/Dashboard.jsx
@@ -4,16 +4,19 @@ const Dashboard = ({ setAuth }) => {
 
     const [email, setEmail] = useState("");
 
+    const myHeaders = {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${localStorage.token || ""}`
+    }
+
     const displayEmail = async () => {
         try {
             const response = await fetch("http://localhost:3500/dashboard/", {
                 method: "GET",
-                headers: { token: localStorage.token } 
+                headers: myHeaders
             });
 
             const parseResponse = await response.json();
-
-            console.log(parseResponse);
 
             setEmail(parseResponse.user_email);
             
@@ -40,12 +43,36 @@ const Dashboard = ({ setAuth }) => {
         }
     };
 
-    return (
-        <div className="dashboardCtn">
-            <h1>Welcome, {email}</h1>
-            <button onClick={logoutUser}>Log out</button>
-        </div>
+    const searchWeather = async () => {
+       try {
 
+            const city = document.getElementById("cityInput").value;
+
+            const response = await fetch(`http://localhost:3500/weather?city=${encodeURIComponent(city)}`, {
+                method: "GET",
+                headers: myHeaders
+            });
+
+            const parseResponse = await response.json();
+            console.log(parseResponse);
+        
+       } catch (error) {
+        console.error(error.message);
+       }
+    };
+
+    return (
+        <>
+            <div className="dashboardCtn">
+                <h1>Welcome, {email}</h1>
+                <button onClick={logoutUser}>Log out</button>
+            </div>
+            <div>
+                <input type="text" id="cityInput" placeholder="Enter city" required></input>
+                <button type="submit" onClick={searchWeather}>Search Weather</button>
+            </div>
+
+        </>
     );
 }
 
