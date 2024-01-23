cd9f6113121e2296b556de851b72c687b3dec55b
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-23 12:27:11 -0500

    removed slice from mapping weatherData

--- client/src/components/auth/Weather.jsx
+++ client/src/components/auth/Weather.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+const formatDate = (dateString) => {
+  const dateObject = new Date(dateString);
+  const month = dateObject.toLocaleString('en-US', { month: 'short' });
+  const day = dateObject.getDate();
+  return `${month} ${day}`;
+};
+
 const Weather = ({ weatherData }) => {
   if (!weatherData) {
     return null;
@@ -8,16 +15,18 @@ const Weather = ({ weatherData }) => {
   return (
     <div className="wrapper">
       <h1>{weatherData.city.name}</h1>
-      <ul>{weatherData.list.slice(0, 5).map((list) => {
+      <ul>{weatherData.list.map((list) => {
         return(
-          <li>
-            <p>Date: {list.dt_txt}</p>
+          <li key={list.dt}>
+            <p>Date: {formatDate(list.dt_txt)}</p>
             <p>{list.main.temp}°C</p>
+            <p>{list.weather[0].description}</p>
             <p>Feels like: {list.main.feels_like}°C</p>
             <p>H: {list.main.temp_max}°C</p>
             <p>L: {list.main.temp_min}°C</p>
             <p>Humidity: {list.main.humidity}%</p>
             <p>Pressure: {list.main.pressure}hPa</p>
+            <p>Wind speed: {list.wind.speed}km/h</p>
           </li>
         )
       } )}</ul>
