97f1fb6e3f8f196f368ec6ed91bc60ec48920918
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-23 11:47:48 -0500

    extracted/rendering weather info of interest

--- client/src/components/auth/Weather.jsx
+++ client/src/components/auth/Weather.jsx
@@ -11,7 +11,13 @@ const Weather = ({ weatherData }) => {
       <ul>{weatherData.list.slice(0, 5).map((list) => {
         return(
           <li>
-            <p>{list.main.temp}</p>
+            <p>Date: {list.dt_txt}</p>
+            <p>{list.main.temp}°C</p>
+            <p>Feels like: {list.main.feels_like}°C</p>
+            <p>H: {list.main.temp_max}°C</p>
+            <p>L: {list.main.temp_min}°C</p>
+            <p>Humidity: {list.main.humidity}%</p>
+            <p>Pressure: {list.main.pressure}hPa</p>
           </li>
         )
       } )}</ul>
