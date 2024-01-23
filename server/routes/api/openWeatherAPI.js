8a9176f7ca8f4a3fb01279320d63bbf2bdce2712
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-23 11:36:44 -0500

    calling 5-day forecast API

--- server/routes/api/openWeatherAPI.js
+++ server/routes/api/openWeatherAPI.js
@@ -4,31 +4,67 @@ const authorization = require("../../middleware/authorization");
 
 router.use(authorization);
 
-router.get("/", async (req, res) => {
+//daily weather
+// router.get("/", async (req, res) => {
+//     try {
+//         //dynamically import 'node-fetch' as an ECMAScript Module
+//         const { default: fetch } = await import("node-fetch");
+        
+//         //get the city name from the req.query
+//         //not req.body because this is not a POST request
+//         const { city } = req.query;
+
+//         //check that input field is not empty
+//         if (!city) {
+//             return res.status(400).json({ message: "This field is required." });
+//         }
+
+//         //openWeather API key
+//         const API_KEY = process.env.API_KEY;
+
+//         //construct the API URL with the actual city name and API key
+//         const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`;
+
+//         console.log(apiUrl);
+
+//         const response = await fetch(apiUrl);
+
+//         //check if the response is successful
+//         if (!response.ok) {
+//             throw new Error(`OpenWeather API request failed with status: ${response.status}`);
+//         }
+
+//         const parseResponse = await response.json();
+//         console.log(parseResponse);
+
+//         res.json(parseResponse);
+//     } catch (error) {
+//         console.error(error.message);
+//         return res.status(500).json({ message: `Internal server error.` });
+//     }
+// });
+
+//5-day forecast
+
+router.get("/", async(req, res) => {
     try {
         //dynamically import 'node-fetch' as an ECMAScript Module
         const { default: fetch } = await import("node-fetch");
-        
-        //get the city name from the req.query
-        //not req.body because this is not a POST request
+
         const { city } = req.query;
 
-        //check that input field is not empty
         if (!city) {
             return res.status(400).json({ message: "This field is required." });
         }
 
-        //ppenWeather API key
         const API_KEY = process.env.API_KEY;
 
-        //construct the API URL with the actual city name and API key
-        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`;
+        const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`;
 
         console.log(apiUrl);
 
         const response = await fetch(apiUrl);
 
-        //check if the response is successful
         if (!response.ok) {
             throw new Error(`OpenWeather API request failed with status: ${response.status}`);
         }
@@ -37,10 +73,12 @@ router.get("/", async (req, res) => {
         console.log(parseResponse);
 
         res.json(parseResponse);
+
+
     } catch (error) {
         console.error(error.message);
-        return res.status(500).json({ message: `Internal server error.` });
+        return res.status(500).json({ message: `Internal server error.` })
     }
-});
+})
 
 module.exports = router;
