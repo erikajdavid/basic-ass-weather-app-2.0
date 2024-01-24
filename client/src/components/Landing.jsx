14483a97bc4357bcc6a8e4fbc01b114620f5466f
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-23 21:22:07 -0500

    footer

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -3,13 +3,19 @@ import { Link } from "react-router-dom";
 const Landing = () => {
 
     return (
-        <div className="landingCtn wrapper">
-            <h1>My basic-ass weather app</h1>
-            <p>A real-time global weather app that is nowhere near as complex as the one you're already using.</p>
-            <p>Your weather forecast awaits.</p>
-            
-            <Link to="/signup"><button>Register now</button></Link>
-        </div>
+        <>
+            <div className="landingCtn wrapper">
+                <h1>My basic-ass weather app</h1>
+                <p>A real-time global weather app that is nowhere near as complex as the one you're already using.</p>
+                <p>Your weather forecast awaits.</p>
+                
+                <Link to="/signup"><button>Register now</button></Link>
+            </div>
+            <footer>
+                <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank">Erika David</a></p>
+                <p>All rights reserved © 2024</p>
+            </footer>
+        </>
     );
 }
 
