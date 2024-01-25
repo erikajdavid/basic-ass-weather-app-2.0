54dd812208664b6fb92d103babd09e1b5ae5ae44
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-24 20:38:19 -0500

    commit

--- client/src/components/auth/Weather.jsx
+++ client/src/components/auth/Weather.jsx
@@ -14,36 +14,37 @@ const roundTemperature = (temperature) => {
 };
 
 const Weather = ({ weatherData }) => {
-
   if (!weatherData) {
     return null;
   }
 
-  //find the forecast entry with the closest time to the current time for the current day
-  const todaysWeather = weatherData.list
-    .filter((entry) => {
-      //filter entries for today
-      const entryDate = new Date(entry.dt_txt).toLocaleDateString();
-      const currentDate = new Date().toLocaleDateString();
-      return entryDate === currentDate;
-    })
-    .reduce((closest, entry) => {
-      const forecastTime = new Date(entry.dt_txt).getHours();
-      const currentDiff = Math.abs(forecastTime - currentHours);
-      const closestDiff = Math.abs(new Date(closest.dt_txt).getHours() - currentHours);
+  // const currentHours = new Date().getHours();
 
-      //if this entry is closer to the current time, update the closestWeather
-      if (currentDiff < closestDiff) {
-        return entry;
-      }
+  // Find the forecast entry with the closest time to the current time for the current day
+  // const todaysWeather = weatherData.list
+  //   .filter((entry) => {
+  //     // Filter entries for today
+  //     const entryDate = new Date(entry.dt_txt).toLocaleDateString();
+  //     const currentDate = new Date().toLocaleDateString();
+  //     return entryDate === currentDate;
+  //   })
+  //   .reduce((closest, entry) => {
+  //     const forecastTime = new Date(entry.dt_txt).getHours();
+  //     const currentDiff = Math.abs(forecastTime - currentHours);
+  //     const closestDiff = Math.abs(new Date(closest.dt_txt).getHours() - currentHours);
 
-      return closest;
-    }, weatherData.list.find((entry) => {
-      //fallback to the first entry if no matching time is found
-      return new Date(entry.dt_txt).toLocaleDateString() === new Date().toLocaleDateString();
-    }));
+  //     // If this entry is closer to the current time, update the closestWeather
+  //     if (currentDiff < closestDiff) {
+  //       return entry;
+  //     }
 
-  //filter the forecast entries for the other four days at 12:00 pm
+  //     return closest;
+  //   }, weatherData.list.find((entry) => {
+  //     // Fallback to the first entry if no matching time is found
+  //     return new Date(entry.dt_txt).toLocaleDateString() === new Date().toLocaleDateString();
+  //   }));
+
+  // Filter the forecast entries for the other four days at 12:00 pm
   const forecast = weatherData.list.filter((entry) => {
     const forecastTime = new Date(entry.dt_txt).getHours();
     return forecastTime === 12;
@@ -53,37 +54,31 @@ const Weather = ({ weatherData }) => {
     <div className="wrapper">
       <h1>{weatherData.city.name}</h1>
       <ul>
-        {todaysWeather && (
+        {/* {todaysWeather && (
           <li key={todaysWeather.dt}>
             <p>Date: {formatDate(todaysWeather.dt_txt)}</p>
-            <p>{roundTemperature(todaysWeather.main.temp)}°C</p>
-            <p>{todaysWeather.weather[0].description}</p>
-            <p>Feels like: {roundTemperature(todaysWeather.main.feels_like)}°C</p>
-            <p>H: {roundTemperature(todaysWeather.main.temp_max)}°C</p>
-            <p>L: {roundTemperature(todaysWeather.main.temp_min)}°C</p>
+            <p>Main Temp: {roundTemperature(todaysWeather.main.temp)}°C</p>
+            <p>Description: {todaysWeather.weather[0].description}</p>
+            <p>Feels Like: {roundTemperature(todaysWeather.main.feels_like)}°C</p>
+            <p>High: {roundTemperature(todaysWeather.main.temp_max)}°C</p>
+            <p>Low: {roundTemperature(todaysWeather.main.temp_min)}°C</p>
             <p>Humidity: {todaysWeather.main.humidity}%</p>
             <p>Pressure: {todaysWeather.main.pressure}hPa</p>
-            <p>Wind speed: {todaysWeather.wind.speed}km/h</p>
+            <p>Wind Speed: {todaysWeather.wind.speed}km/h</p>
           </li>
-        )}
+        )} */}
 
         {forecast.map((entry) => (
           <li key={entry.dt}>
             <p>Date: {formatDate(entry.dt_txt)}</p>
-            <p>{roundTemperature(entry.main.temp)}°C</p>
-            <p>{entry.weather[0].description}</p>
-            <p>Feels like: {roundTemperature(entry.main.feels_like)}°C</p>
-            <p>H: {roundTemperature(entry.main.temp_max)}°C</p>
-            <p>L: {roundTemperature(entry.main.temp_min)}°C</p>
-            <p>Humidity: {entry.main.humidity}%</p>
-            <p>Pressure: {entry.main.pressure}hPa</p>
-            <p>Wind speed: {entry.wind.speed}km/h</p>
+            <p>Main Temp: {roundTemperature(entry.main.temp)}°C</p>
+            <p>Description: {entry.weather[0].description}</p>
+            <p>Feels Like: {roundTemperature(entry.main.feels_like)}°C</p>
           </li>
         ))}
-        
       </ul>
     </div>
   );
-}
+};
 
 export default Weather;
