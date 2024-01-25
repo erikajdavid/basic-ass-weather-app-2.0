326d3fae7b8edfd9acf52c9f4e50b4f023cbe579
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-25 10:40:29 -0500

    styling for landing page and dashboard header

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -4,14 +4,19 @@ const Landing = () => {
 
     return (
         <>
-            <div className="landingCtn wrapper">
+            <header>
+                <nav className="wrapper">
+                    <Link to="/">LOGO</Link>
+                    <Link to="/login"><button>Log in</button></Link>
+                </nav>
+            </header>
+            <main className="landingCtn wrapper">
                 <h1>My basic-ass weather app</h1>
                 <p>A real-time global weather app that is nowhere near as complex as the one you're already using.</p>
                 <p>Your weather forecast awaits.</p>
-                
                 <Link to="/signup"><button>Register now</button></Link>
-            </div>
-            <footer>
+            </main>
+            <footer className="wrapper">
                 <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank">Erika David</a></p>
                 <p>All rights reserved © 2024</p>
             </footer>
