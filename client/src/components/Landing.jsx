bff3b9d3e57abd403798f11ef1deb6de44a6d512
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-26 12:55:58 -0500

    landing text spacing

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -12,8 +12,10 @@ const Landing = () => {
             </header>
             <main className="landingCtn wrapper">
                 <h1>My basic-ass weather app</h1>
-                <p>A real-time global weather app that is nowhere near as complex as the one you're already using.</p>
-                <p>Your weather forecast awaits.</p>
+                <div className="landingText">
+                    <p>A real-time global weather app that is nowhere near as complex as the one you're already using.</p>
+                    <p>Your weather forecast awaits.</p>
+                </div>
                 <Link to="/signup"><button>Register now</button></Link>
             </main>
             <footer className="wrapper">
