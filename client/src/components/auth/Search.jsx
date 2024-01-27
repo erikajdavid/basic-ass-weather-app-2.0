20c3d94456e5f9f352b768627fc26b71d961e4ff
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-27 13:32:38 -0500

    redid styling for search box, updated semantics

--- client/src/components/auth/Search.jsx
+++ client/src/components/auth/Search.jsx
@@ -14,23 +14,22 @@ const Search = ({ handleSearch }) => {
   }
 
   return (
-    <>
-      <div className="searchWrapper">
-        <input type="text" 
-          id="cityInput" 
-          placeholder="Enter a city..." 
-          value={city}
-          onChange={(e => setCity(e.target.value))}
-          required></input>
-        <button 
-          type="submit" 
-          onClick={handleSearchClick}
-          className={!city ? "buttonDisabled" : "buttonEnabled"}>
-            {/* adding FA like this here because this icon isn't available in the FA library installed */}
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
-      </div>
-    </>
+    <form className="searchWrapper">
+      <input type="text" 
+        id="cityInput" 
+        placeholder="Enter a city..." 
+        value={city}
+        onChange={(e => setCity(e.target.value))}
+        required>
+      </input>
+      <button 
+        type="submit" 
+        onClick={handleSearchClick}
+        className={!city ? "buttonDisabled" : "buttonEnabled"}>
+          {/* adding FA like this here because this icon isn't available in the FA library installed */}
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </form>
   );
 }
 
