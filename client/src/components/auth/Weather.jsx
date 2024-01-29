656e0fc0f65e3871a2b5778546aa84e5a02da916
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-29 18:38:33 -0500

    daily weather description

--- client/src/components/auth/Weather.jsx
+++ client/src/components/auth/Weather.jsx
@@ -25,7 +25,7 @@ const Weather = ({ dailyWeather, forecastWeather }) => {
       <div>
         <h2>Daily Forecast:</h2>
         <p>Temp: {roundTemperature(dailyWeather.main.temp)}°C</p>
-        <p>Description: {dailyWeather.weather.description}</p>
+        <p>Description: {dailyWeather.weather[0].description}</p>
         <p>Feels Like: {roundTemperature(dailyWeather.main.feels_like)}°C</p>
         <p>H: {roundTemperature(dailyWeather.main.temp_max)}°C</p>
         <p>L: {roundTemperature(dailyWeather.main.temp_min)}°C</p>
