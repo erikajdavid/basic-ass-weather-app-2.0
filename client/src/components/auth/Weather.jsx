4f2d23ebffe963e42055392c9c9f9f2c648c0530
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-29 17:49:15 -0500

    rendering weather data from two APIs

--- client/src/components/auth/Weather.jsx
+++ client/src/components/auth/Weather.jsx
@@ -13,17 +13,30 @@ const roundTemperature = (temperature) => {
   return Math.round(temperature);
 };
 
-const Weather = ({ weatherData }) => {
+const Weather = ({ dailyWeather, forecastWeather }) => {
 
-  if (!weatherData) {
+  if (!dailyWeather || !forecastWeather) {
     return null;
   }
 
   return (
     <div className="wrapper">
-      <h1>{weatherData.city.name}</h1>
-      <ul>
-        {weatherData.list.map((day) => (
+      <h1>{dailyWeather.name}</h1>
+      <div>
+        <h2>Daily Forecast:</h2>
+        <p>Temp: {roundTemperature(dailyWeather.main.temp)}°C</p>
+        <p>Description: {dailyWeather.weather.description}</p>
+        <p>Feels Like: {roundTemperature(dailyWeather.main.feels_like)}°C</p>
+        <p>H: {roundTemperature(dailyWeather.main.temp_max)}°C</p>
+        <p>L: {roundTemperature(dailyWeather.main.temp_min)}°C</p>
+        <p>Humidity: {dailyWeather.main.humidity}%</p>
+        <p>Pressure: {dailyWeather.main.pressure}hPa</p>
+        <p>Wind Speed: {dailyWeather.wind.speed}km/h</p>
+      </div>
+      <div>
+        <h2>Next 4-day Forecast</h2>
+        <ul>
+        {forecastWeather.list.map((day) => (
             <li key={day.dt}>
               <p>Date: {formatDate(day.dt_txt)}</p>
               <p>Main Temp: {roundTemperature(day.main.temp)}°C</p>
@@ -34,9 +47,10 @@ const Weather = ({ weatherData }) => {
               <p>Humidity: {day.main.humidity}%</p>
               <p>Pressure: {day.main.pressure}hPa</p>
               <p>Wind Speed: {day.wind.speed}km/h</p>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
