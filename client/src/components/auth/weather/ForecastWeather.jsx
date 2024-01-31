b8ef0183d4c99d2bff6a65e77c473cfdd71d6f22
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-31 16:05:27 -0500

    extracted 12pm weather forecast for the next five days

--- client/src/components/auth/weather/ForecastWeather.jsx
+++ client/src/components/auth/weather/ForecastWeather.jsx
@@ -2,21 +2,38 @@ import React from "react";
 
 const ForecastWeather = ({ forecastWeather, formatDate, roundTemperature }) => {
 
+    const noonWeather = forecastWeather.list
+    .map((entry) => {
+      const entryDateTime = new Date(entry.dt_txt);
+      return entryDateTime.getHours() === 12 && entryDateTime.getMinutes() === 0
+        ? {
+            date: entryDateTime.toISOString(),
+            mainTemp: entry.main.temp,
+            description: entry.weather[0].description,
+            feelsLike: entry.main.feels_like,
+            humidity: entry.main.humidity,
+            pressure: entry.main.pressure,
+            windSpeed: entry.windSpeed,
+          }
+        : null;
+    })
+    .filter(Boolean)
+
+  console.log(noonWeather);
+    
     return (
         <div className="wrapper">
             <h2>Next 4-day Forecast</h2>
             <ul>
-            {forecastWeather.list.map((day) => (
-                <li key={day.dt}>
-                <p>Date: {formatDate(day.dt_txt)}</p>
-                <p>Main Temp: {roundTemperature(day.main.temp)}°C</p>
-                <p>Description: {day.weather[0].description}</p>
-                <p>Feels Like: {roundTemperature(day.main.feels_like)}°C</p>
-                <p>H: {roundTemperature(day.main.temp_max)}°C</p>
-                <p>L: {roundTemperature(day.main.temp_min)}°C</p>
-                <p>Humidity: {day.main.humidity}%</p>
-                <p>Pressure: {day.main.pressure}hPa</p>
-                <p>Wind Speed: {day.wind.speed}km/h</p>
+            {noonWeather.map((day) => (
+                <li key={day.date}>
+                <p>Date: {formatDate(day.date)}</p>
+                <p>Main Temp: {roundTemperature(day.mainTemp)}°C</p>
+                <p>Description: {day.description}</p>
+                <p>Feels Like: {roundTemperature(day.feelsLike)}°C</p>
+                <p>Humidity: {day.humidity}%</p>
+                <p>Pressure: {day.pressure}hPa</p>
+                <p>Wind Speed: {day.speed}km/h</p>
                 </li>
             ))}
             </ul>
