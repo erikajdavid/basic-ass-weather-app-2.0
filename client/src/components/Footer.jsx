eafd57bffa600c00e64dabb06849d65d7cafb04a
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-04 15:19:21 -0500

    added title on hover for personal website and social links for accessibility

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ const Footer = () => {
     return (
         <footer className="wrapper">
             <div>
-                <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank">Erika David</a></p>
-                <a href="https://github.com/erikajdavid/" target="_blank" className="social"><i class="devicon-github-original"></i></a>
-                <a href="https://www.linkedin.com/in/erikadaviddev/" target="_blank" className="social"><i class="devicon-linkedin-plain"></i></a>
+                <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank" title="visit my website">Erika David</a></p>
+                <a href="https://github.com/erikajdavid/" target="_blank" title="my github"className="social"><i class="devicon-github-original"></i></a>
+                <a href="https://www.linkedin.com/in/erikadaviddev/" target="_blank" title="my Linkedin" className="social"><i class="devicon-linkedin-plain"></i></a>
             </div>
             <p>All rights reserved © 2024</p>
         </footer>
