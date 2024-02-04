b71d3f64b602e5123a6c7beee526844be69988c9
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-04 15:13:50 -0500

    updated footer semantics/content, added/linked social media

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
 
     return (
         <footer className="wrapper">
-            <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank">Erika David</a></p>
+            <div>
+                <p>Designed and developed by <a href="https://erikadavid.dev/" target="_blank">Erika David</a></p>
+                <a href="https://github.com/erikajdavid/" target="_blank" className="social"><i class="devicon-github-original"></i></a>
+                <a href="https://www.linkedin.com/in/erikadaviddev/" target="_blank" className="social"><i class="devicon-linkedin-plain"></i></a>
+            </div>
             <p>All rights reserved © 2024</p>
         </footer>
     );
