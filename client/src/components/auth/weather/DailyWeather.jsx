f21204d0ace699c4769a4473d2d8a4c02e594538
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-05 11:05:38 -0500

    extracted sunrise/sunset data from API, refactored code

--- client/src/components/auth/weather/DailyWeather.jsx
+++ client/src/components/auth/weather/DailyWeather.jsx
@@ -13,6 +13,12 @@ const DailyWeather = ({ dailyWeather, roundTemperature, capitalize }) => {
     const today = new Date();
     const formatDate = format(today, 'EEEE, MMMM d, yyyy');
 
+    const formatTime = (timestamp) => {
+        const time = new Date(timestamp * 1000);
+        const formattedTime = format(time, 'h:ss');
+        return formattedTime;
+    }
+
     return (
         <div className="dailyWeatherCtn">
             <div>
@@ -24,6 +30,12 @@ const DailyWeather = ({ dailyWeather, roundTemperature, capitalize }) => {
                 <p>Feels Like: {roundTemperature(dailyWeather.main.feels_like)}°C</p>
                 <img src={`http://openweathermap.org/img/w/${icon}.png`} alt={dailyWeather.weather[0].description}/>
                 <p className="weatherDescription">{capitalize(dailyWeather.weather[0].description)}</p>
+            </div>
+            <div>
+                <p>Sunrise: {formatTime(dailyWeather.sys.sunrise)} AM</p>
+                <p>Sunset: {formatTime(dailyWeather.sys.sunset)} PM</p>
+            </div>
+            <div>
                 <p>Humidity: {dailyWeather.main.humidity}%</p>
                 <p>Pressure: {dailyWeather.main.pressure}hPa</p>
                 <p>Wind speed: {dailyWeather.wind.speed}km/h</p>
