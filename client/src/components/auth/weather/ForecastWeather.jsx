90520386558bbb6cb45b1be82cda557ee85c9d6b
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-07 13:40:50 -0500

    rendering your own weather icons yay

--- client/src/components/auth/weather/ForecastWeather.jsx
+++ client/src/components/auth/weather/ForecastWeather.jsx
@@ -18,22 +18,45 @@ const ForecastWeather = ({ forecastWeather, formatDate, roundTemperature, capita
           description: entry.weather[0].description,
           icon: entry.weather[0].icon,
           feelsLike: entry.main.feels_like,
+          id: entry.weather[0].id
         }
       : null;
   })
   .filter(Boolean)
 
-    
+  console.log(noonWeather)
+
+  const getCustomWeatherIconUrl = (id) => {
+    if (id >= 200 && id < 300) {
+        return "./assets/11d.png";
+    } else if (id >= 300 && id < 40 || id >= 500 && id < 600) {
+        return "./assets/09d.png";
+    } else if (id >= 600 && id < 700) {
+        return "./assets/13d.png";
+    } else if (id >= 700 && id < 800) {
+        return "./assets/50d.png"
+    } else if (id === 800) {
+        return "./assets/01d.png"
+    } else if (id >= 801 && id < 900) {
+        return "./assets/03d.png"
+    }
+  }
+
+const customWeatherIconUrl = noonWeather.map(day => getCustomWeatherIconUrl(day.id));
+console.log(customWeatherIconUrl);    
+
     return (
         <div className="forecastWeatherCtn">
             <h2>Coming up this week</h2>
             <ul>
-            {noonWeather.map((day) => (
+            {noonWeather.map((day, index) => (
                 <li key={day.date} className="forecastDayCtn">
                     <p>{formatDate(day.date)}</p>
                     <p className="temp">{roundTemperature(day.mainTemp)}°C <span>/ {roundTemperature(day.feelsLike)}°C</span></p>
                     {/* <p>{capitalize(day.description)}</p> */}
-                    <img src={`http://openweathermap.org/img/w/${day.icon}.png`} alt={day.description}/>
+                    <div className="forecastImgCtn">
+                      <img src={customWeatherIconUrl[index]} alt={day.description} />
+                    </div>
                 </li>
             ))}
             </ul>
