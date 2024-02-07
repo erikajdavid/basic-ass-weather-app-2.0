538c0ec63460b65ec5a4db88d5aa0f1ccfd482bc
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-07 18:21:06 -0500

    updated conditional rendering of icons with more icons

--- client/src/components/auth/weather/ForecastWeather.jsx
+++ client/src/components/auth/weather/ForecastWeather.jsx
@@ -28,16 +28,20 @@ const ForecastWeather = ({ forecastWeather, formatDate, roundTemperature, capita
 
   const getCustomWeatherIconUrl = (id) => {
     if (id >= 200 && id < 300) {
-        return "./assets/11d.png";
-    } else if (id >= 300 && id < 400 || id >= 500 && id < 600) {
+      return "./assets/11d.png";
+    } else if (id >= 300 && id < 400) {
         return "./assets/09d.png";
+    } else if (id >= 500 && id < 600) {
+        return "./assets/04d.png";
     } else if (id >= 600 && id < 700) {
         return "./assets/13d.png";
     } else if (id >= 700 && id < 800) {
         return "./assets/50d.png"
     } else if (id === 800) {
         return "./assets/01d.png"
-    } else if (id >= 801 && id < 900) {
+    } else if (id === 801) {
+        return "./assets/02d.png"
+    } else if (id >= 802 && id < 900) {
         return "./assets/03d.png"
     }
   }
