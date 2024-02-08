4d36bc4f2251429f24f92b43d372b5d957c26d76
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-08 18:28:53 -0500

    removed console.logs and imported component for conditional rendering of weather icons

--- client/src/components/auth/weather/DailyWeather.jsx
+++ client/src/components/auth/weather/DailyWeather.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { format } from "date-fns";
+import { getCustomWeatherIconUrl } from "./WeatherUtils";
 
 const DailyWeather = ({ dailyWeather, roundTemperature, capitalize }) => {
 
@@ -23,26 +24,6 @@ const DailyWeather = ({ dailyWeather, roundTemperature, capitalize }) => {
         const hours = Math.floor(dayLengthInSeconds / 3600);
         const minutes = Math.floor((dayLengthInSeconds % 3600) / 60);
         return `${hours}h ${minutes}min`;
-    };
-
-    const getCustomWeatherIconUrl = (id) => {
-        if (id >= 200 && id < 300) {
-            return "./assets/11d.png";
-        } else if (id >= 300 && id < 400) {
-            return "./assets/09d.png";
-        } else if (id >= 500 && id < 600) {
-            return "./assets/04d.png";
-        } else if (id >= 600 && id < 700) {
-            return "./assets/13d.png";
-        } else if (id >= 700 && id < 800) {
-            return "./assets/50d.png"
-        } else if (id === 800) {
-            return "./assets/01d.png"
-        } else if (id === 801) {
-            return "./assets/02d.png"
-        } else if (id >= 802 && id < 900) {
-            return "./assets/03d.png"
-        }
     };
 
     const customWeatherIconUrl = getCustomWeatherIconUrl(id);
--- client/src/components/auth/weather/ForecastWeather.jsx
+++ client/src/components/auth/weather/ForecastWeather.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { getCustomWeatherIconUrl } from "./WeatherUtils";
 
 const ForecastWeather = ({ forecastWeather, formatDate, roundTemperature, capitalize }) => {
 
   if (!forecastWeather || !forecastWeather.list || forecastWeather.list.length === 0) {
     return null;
   }
-
-  console.log(forecastWeather);
 
   const noonWeather = forecastWeather.list
   .map((entry) => {
@@ -24,30 +23,7 @@ const ForecastWeather = ({ forecastWeather, formatDate, roundTemperature, capita
   })
   .filter(Boolean)
 
-  console.log(noonWeather)
-
-  const getCustomWeatherIconUrl = (id) => {
-    if (id >= 200 && id < 300) {
-      return "./assets/11d.png";
-    } else if (id >= 300 && id < 400) {
-        return "./assets/09d.png";
-    } else if (id >= 500 && id < 600) {
-        return "./assets/04d.png";
-    } else if (id >= 600 && id < 700) {
-        return "./assets/13d.png";
-    } else if (id >= 700 && id < 800) {
-        return "./assets/50d.png"
-    } else if (id === 800) {
-        return "./assets/01d.png"
-    } else if (id === 801) {
-        return "./assets/02d.png"
-    } else if (id >= 802 && id < 900) {
-        return "./assets/03d.png"
-    }
-  }
-
-const customWeatherIconUrl = noonWeather.map(day => getCustomWeatherIconUrl(day.id));
-console.log(customWeatherIconUrl);    
+  const customWeatherIconUrl = noonWeather.map(day => getCustomWeatherIconUrl(day.id));
 
     return (
         <div className="forecastWeatherCtn">
