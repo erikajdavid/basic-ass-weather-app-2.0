41e3f025fef6e95f07cdb226d5f1040dc553109c
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-09 16:50:55 -0500

    able to toggle save city icon

--- client/src/components/auth/SaveCity.jsx
+++ client/src/components/auth/SaveCity.jsx
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
+
 const SaveCity = () => {
+    const [isSaved, setIsSaved] = useState(false);
+
+    const handleSaveClick = () => {
+        setIsSaved(!isSaved);
+    };
 
     return (
-        <div className="saveCity">
-            <i class="fa-regular fa-heart"></i>
+        <div className="saveCity" onClick={handleSaveClick}>
+            {isSaved ? (
+                <i className="fa-solid fa-heart"></i>
+            ) : (
+                <i className="fa-regular fa-heart"></i>
+            )}
         </div>
     );
-
-}
+};
 
 export default SaveCity;
