fd14cfbdd50eb1615f2bcee4762dc94c2c40f387
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-12 11:18:18 -0500

    handleToggleUnit function lives here

--- client/src/components/auth/weather/WeatherCard.jsx
+++ client/src/components/auth/weather/WeatherCard.jsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import DailyWeather from "./DailyWeather";
 import ForecastWeather from "./ForecastWeather";
 import { capitalize, roundTemperature } from "./WeatherUtils";
 import SaveCity from "../SaveCity";
+import TempToggle from "../TempToggle";
 
-const WeatherCard = ({ dailyWeather, forecastWeather, handleToggleUnit, unit }) => {
+const WeatherCard = ({ dailyWeather, forecastWeather }) => {
+
+  const [unit, setUnit] = useState("metric");
 
   if (!dailyWeather || !forecastWeather) {
     return null;
   }
 
+  const handleToggleUnit = () => {
+    // Toggle between Celsius and Fahrenheit
+    setUnit(unit === 'metric' ? 'imperial' : 'metric');
+  };  
+
+  const convertTemperature = (temperature) => {
+    if (unit === 'metric') {
+        return roundTemperature(temperature);
+    } else {
+        // Convert Celsius to Fahrenheit
+        return roundTemperature((temperature * 9/5) + 32);
+    }
+};
+
   return (
     <div className="weatherCardMainCtn">
       <SaveCity />
       <div className="weatherCardCtn wrapper">  
+        <TempToggle handleToggleUnit={handleToggleUnit} unit={unit}/>
         <DailyWeather 
           roundTemperature={roundTemperature} 
           dailyWeather={dailyWeather} 
           capitalize={capitalize}
-          handleToggleUnit={handleToggleUnit}
-          unit={unit} 
+          unit={unit}
+          convertTemperature={convertTemperature}
         >  
         </DailyWeather>
 
         <ForecastWeather 
           roundTemperature={roundTemperature} 
           forecastWeather={forecastWeather} 
+          unit={unit}
+          convertTemperature={convertTemperature}
         >  
         </ForecastWeather>
       </div>
