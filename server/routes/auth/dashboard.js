bbe187affd410524d3336aef3a4a85c7ecf3d4f8
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-13 16:39:56 -0500

    created endpoint for saving a city and tested API with Postman

--- server/routes/auth/dashboard.js
+++ server/routes/auth/dashboard.js
@@ -13,5 +13,33 @@ router.get("/", authorization, async (req, res) => {
         console.error(error.message);
         return res.status(500).json({ message: `Server error.` });
     }
-})
+});
+
+router.post("/favorite_city", authorization, async(req, res) => {
+    const { city_name } = req.body;
+    const user_id = req.user.id; // Get user ID from the authenticated user
+
+    try {
+        // Retrieve the user_id from your database
+        const userResult = await pool.query('SELECT u.user_id FROM users u LEFT JOIN favorites f ON u.user_id = f.user_id WHERE u.user_id = $1', [user_id]);
+        
+        // Check if user_id exists in the database
+        if (userResult.rows.length === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const getUserId = userResult.rows[0].user_id;
+
+        const result = await pool.query('INSERT INTO favorites (user_id, city_name) VALUES ($1, $2) RETURNING *', [getUserId, city_name]);
+
+        const savedCity = result.rows[0];
+
+        res.json({ user_id: savedCity.user_id, city_name: savedCity.city_name });
+
+    } catch (error) {
+        console.error('Error saving favorite city:', error);
+        res.status(500).json({ error: 'An error occurred while saving favorite city' });
+    }
+});
+
 module.exports = router;
