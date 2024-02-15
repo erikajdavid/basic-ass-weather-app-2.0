dc8d3c4b214b2066b94eadf8788335af256bc8ad
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-15 14:49:48 -0500

    created conditional for rendering day or night weather icons depending on time of day

--- client/src/components/auth/weather/WeatherUtils.jsx
+++ client/src/components/auth/weather/WeatherUtils.jsx
@@ -1,7 +1,50 @@
 import { format } from "date-fns";
 
-//for rendering your custom icons
-export const getCustomWeatherIconUrl = (id) => {
+export const getCustomWeatherIconUrlDaily = (id, sunrise, sunset) => {
+    const currentTime = new Date().getTime() / 1000; // Current time in seconds
+    const isDaytime = currentTime > sunrise && currentTime < sunset;
+    
+    if (isDaytime) {
+        if (id >= 200 && id < 300) {
+            return "./assets/11d.png";
+        } else if (id >= 300 && id < 400) {
+            return "./assets/09d.png";
+        } else if (id >= 500 && id < 600) {
+            return "./assets/04d.png";
+        } else if (id >= 600 && id < 700) {
+            return "./assets/13d.png";
+        } else if (id >= 700 && id < 800) {
+            return "./assets/50d.png"
+        } else if (id === 800) {
+            return "./assets/01d.png"
+        } else if (id === 801) {
+            return "./assets/02d.png"
+        } else if (id >= 802 && id < 900) {
+            return "./assets/03d.png"
+        }
+
+    } else {
+        if (id >= 200 && id < 300) {
+            return "./assets/11n.png";
+        } else if (id >= 300 && id < 400) {
+            return "./assets/09n.png";
+        } else if (id >= 500 && id < 600) {
+            return "./assets/09n.png"; // Nighttime rain icon
+        } else if (id >= 600 && id < 700) {
+            return "./assets/13n.png";
+        } else if (id >= 700 && id < 800) {
+            return "./assets/50n.png"
+        } else if (id === 800) {
+            return "./assets/01n.png"
+        } else if (id === 801) {
+            return "./assets/02n.png"
+        } else if (id >= 802 && id < 900) {
+            return "./assets/03n.png"
+        }
+    }
+};
+
+export const getCustomWeatherIconUrlForecast = (id) => {
     if (id >= 200 && id < 300) {
         return "./assets/11d.png";
     } else if (id >= 300 && id < 400) {
