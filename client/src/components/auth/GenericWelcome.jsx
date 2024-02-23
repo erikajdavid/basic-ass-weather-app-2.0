54edc30914326c4b3396ff1bda2ee4206c270c99
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-02-22 23:13:48 -0500

    updated with basic elements, functional handleNowClick

--- client/src/components/auth/GenericWelcome.jsx
+++ client/src/components/auth/GenericWelcome.jsx
@@ -1,9 +1,21 @@
-const GenericWelcome = () => {
+const GenericWelcome = ({ cityInputRef }) => {
+
+    const handleNowClick = () => {
+        if (cityInputRef.current) {
+            cityInputRef.current.focus();
+        } else {
+            console.error("cityInputRef is null or not yet initialized.");
+        }
+    }
 
     return (
-
-        <p>Welcome to your basic-ass weather app.</p>
-
+        <div className="genericCtn">
+            <div>
+                <p>Welcome to your basic-ass weather app!</p>
+                <p>Get your daily and forecast weather <span onClick={handleNowClick} className="now">now</span>.</p>
+            </div>
+            <img src="./assets/04d.png" alt="weather icon"/>
+        </div> 
     );
 
 }
